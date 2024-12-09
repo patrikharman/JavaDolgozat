d4646272c47c47354b12d7025ec3ace7577dfd61
Harman Patrik <harman.patrik@dk.akkszalezi.hu>
2024-12-09 13:52:42 +0100

    f3,f4

--- src/main/java/hu/szamalk/Main.java
+++ src/main/java/hu/szamalk/Main.java
@@ -1,7 +1,11 @@
 package hu.szamalk;
 
+import hu.szamalk.modell.Tabla;
+
 public class Main {
     public static void main(String[] args) {
+        Tabla T = new Tabla('#');
+        T.Megjelenit();
 
     }
 }
--- src/main/java/hu/szamalk/modell/Tabla.java
+++ src/main/java/hu/szamalk/modell/Tabla.java
@@ -1,15 +1,26 @@
 package hu.szamalk.modell;
 
+import java.util.Arrays;
+
 public class Tabla {
     private char[][] T;
     private char UresCella;
 
-    public Tabla(char[][] T, char UresCella) {
+    public Tabla(char UresCella) {
         this.T = new char[8][8];
         this.UresCella = UresCella;
 
+        feltolt();
+
 
     }
+
+    private void feltolt() {
+        for (int i = 0; i < T.length; i++) {
+            Arrays.fill(T[i], UresCella);
+        }
+    }
+
 
     public void Elhelyez(){
 
@@ -20,6 +31,12 @@ public class Tabla {
     }
 
     public void Megjelenit(){
+        for (char[] sor : T) {
+            for (char cella : sor) {
+                System.out.print(cella + "");
+            }
+            System.out.println();
+        }
 
     }
 
