89999b2eb8f7bb25272756f3c4642692fdbeb468
Harman Patrik <harman.patrik@dk.akkszalezi.hu>
2024-12-09 13:34:49 +0100

    f1,f2

--- src/main/java/hu/szamalk/Main.java
+++ src/main/java/hu/szamalk/Main.java
@@ -2,6 +2,6 @@ package hu.szamalk;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
---
+++ src/main/java/hu/szamalk/modell/Tabla.java
@@ -0,0 +1,37 @@
+package hu.szamalk.modell;
+
+public class Tabla {
+    private char[][] T;
+    private char UresCella;
+
+    public Tabla(char[][] T, char UresCella) {
+        this.T = new char[8][8];
+        this.UresCella = UresCella;
+
+
+    }
+
+    public void Elhelyez(){
+
+    }
+
+    public void FajlbaIr(){
+
+    }
+
+    public void Megjelenit(){
+
+    }
+
+    public void Tabla(){
+
+    }
+
+    public void UresOszlop(){
+
+    }
+
+    public void UresSor(){
+
+    }
+}
